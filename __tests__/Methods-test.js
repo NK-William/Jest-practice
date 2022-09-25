7be49084bf0e3732c9e5547ca284e2492666636c
William <NkunaTebogo09@gmail.com>
2022-09-25 19:26:43 +0200

    Fixed path

--- __tests__/Methods-test.js
+++
@@ -1,3 +0,0 @@
-import {sum} from '../src/Methods';
-
-it("Test summation", () => expect(sum(1,2)).toEqual(3));
---
+++ __tests__/sum-test.js
@@ -0,0 +1,3 @@
+import {sum} from '../src/Functions/sum';
+
+it('Test summation', () => expect(sum(1, 2)).toEqual(3));
